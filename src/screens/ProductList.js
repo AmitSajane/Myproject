5f8f481bc9bda44ff3b8a87b3e5e6850eb6e390d
Amit Sajane <amitsajane5284@gmail.com>
2021-12-31 13:34:01 +0530

    added star-rating

--- src/screens/ProductList.js
+++ src/screens/ProductList.js
@@ -1,139 +1,86 @@
-import React, {useEffect, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-  Image,
-  ActivityIndicator
-} from 'react-native';
-import Header from '../componentes/HeaderComponent';
-import colors from '../shared/colors';
+import React from 'react';
+import {StyleSheet, Text, View, FlatList, Image, TouchableOpacity} from 'react-native';
+import Product1 from '../assets/images/product1.jpg';
+import Product2 from '../assets/images/product2.jpg' ;
+import Product3 from '../assets/images/product3.jpg';
+import Kids from '../assets/images/kids.png';
 import {scale} from '../shared/scaling';
-import StarRating from 'react-native-star-rating';
+import {useNavigation} from '@react-navigation/native';
 
-//  import dummyApi from '../shared/dummyApi';
+const DATA = [
+  {
+    id: '1',
+    title: 'Winter Wear',
+    title2: '40-50% Off',
+    uri: Product1,
+  },
+  {
+    id: '2',
+    title: 'Womens',
+    title2: '50-70% Off',
+    uri: Product2,
+  },
+  {
+    id: '3',
+    title: 'Kids',
+    title2: '40-50% Off',
+    uri: Kids,
+  },
 
-const ProductList = ({navigation}) => {
-  const [products, setProducts] = useState([]);
+  {
+    id: '4',
+    title: 'WinterWear',
+    title2: '40-50% Off',
+    uri: Product3,
+  },
 
-  useEffect(() => {
-    getProductList();
-    setTimeout(() => {
-      console.log('products is ', products);
-    }, 3000);
-    return () => {
-      setProducts({});
-    };
-  }, []);
+];
 
-  const getProductList = async () => {
-    const data = await fetch('https://fakestoreapi.com/products')
-      .then(res => res.json()) // 2
-      .then(resJson => {
-        setProducts(resJson);
-      })
-      .catch(e => console.log(e));
-    console.log('data is ..', JSON.stringify(data, null, 2));
+const Item = ({title, source, offerText, onclick}) => (
+  <TouchableOpacity onPress={onclick} style={styles.item}>
+    <Image style={styles.tinyLogo} source={source} />
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{offerText}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const TopProduct = () => {
+  const navigation = useNavigation();
+  const nextmove = () => {
+    navigation.navigate('ProductList');
+    //  Alert.alert('item is clicked')
   };
-
-  const nextScreen = () => {
-    navigation.navigate('ProductDetails');
-  };
-
   const renderItem = ({item}) => (
-    <TouchableOpacity onPress={nextScreen} style={styles.item}>
-      <Image style={styles.tinyLogo} source={{uri: item.image}} />
-      <View style={styles.container}>
-        <Text numberOfLines={1} style={styles.title}>
-          {item.title}
-        </Text>
-        <Text numberOfLines={1} style={styles.descripation}>
-          {item.description}
-        </Text>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Text
-            numberOfLines={1}
-            style={[styles.descripation, styles.prizetext]}>
-            ${item.price}
-          </Text>
-          <View style={{alignItems:"center",justifyContent:'center',marginLeft:scale(10)}}>
-            <StarRating
-              disabled
-              maxStars={5}
-              rating={3}
-              fullStarColor={'red'}
-              starSize={15}
-              // selectedStar={(rating) => this.onStarRatingPress(rating)}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableOpacity>
+    <Item title={item.title} source={item.uri} offerText={item.title2} onclick={nextmove}/>
   );
-
   return (
-    <View>
-      <Header
-        title={'Products'}
-        arrowBack={true}
-        onPressBack={() => navigation.goBack()}
+    <View style={{flex: 1,width:'100%'}}>
+      <FlatList
+        data={DATA}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
       />
-      <View style={{paddingBottom: scale(85)}}>
-        <FlatList
-          data={products}
-          numColumns={2}
-          showsVerticalScrollIndicator={false}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          ListHeaderComponent={() => (!products.length? 
-            <View>
-            <ActivityIndicator size="large" color={colors.theme} />
-            <Text style={styles.emptyMessageStyle}>Products are loading...</Text>  
-            </View>
-            : null)
-          }
-        />
-      </View>
     </View>
   );
 };
 
-export default ProductList;
+export default TopProduct;
 
 const styles = StyleSheet.create({
-  item: {
-    width: '50%',
-    marginTop: scale(10),
-    // height:20,
-  },
   tinyLogo: {
-    width: '90%',
-    // resizeMode:'contain',
+    width: 185,
     height: 180,
     marginLeft: 6,
   },
   container: {
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    backgroundColor: colors.white,
-    paddingHorizontal: scale(10),
-    paddingVertical: scale(10),
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-    fontSize: scale(16),
+    fontSize: scale(18),
   },
-  descripation: {
-    fontSize: scale(12),
-  },
-  prizetext: {
-    color: '#000',
-    fontSize: scale(14),
-    fontWeight: '400',
-  },
-  emptyMessageStyle: {
-    textAlign: 'center',
-    fontSize:scale(20)
-  }
 });
